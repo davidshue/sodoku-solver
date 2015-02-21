b16b9d874da6790b67b84994d7a0b28b1147c40b
David Shue <dshue1@babycenter.com>
2015-02-20 20:36:51 -0800

    more discovery with @Field in groovy script

--- src/main/java/SodokuSolver.groovy
+++ src/main/java/SodokuSolver.groovy
@@ -1,42 +1,52 @@
-def model = (1..9)
+import groovy.transform.Field
 
-boolean valid = true
-
+@Field final def model = (1..9)
 
 String s= '''123456789123456789123456789123456789123456789123456789123456789123456789123456789'''
 
-valid = valid && s ==~ /\d+/ &&  s.length() == 81
+boolean valid = allNum(s) &&  exact81characters(s) && validSolution(s)
 
-def strings = s.split("(?!^)")
-def ints = strings.collect {it as int}
-
-def rows = ints.collate(9)
-
-rows.each { row ->
-    valid = valid && row.sort() == model
+private boolean allNum(String s) {
+	s ==~ /\d+/
 }
 
-def columns = [].withEagerDefault{[]}
-ints.eachWithIndex {o, i ->
-    int index = i % 9
-    columns[index] << o
-}
-columns.each { column ->
-    valid = valid && column.sort() == model
+private boolean exact81characters(String s) {
+	s.length() == 81
 }
 
-
-def grids = [].withEagerDefault{[]}
-
-ints.eachWithIndex {o, i ->
-    int index = i.intdiv(27) * 3  + (i % 9).intdiv(3)
-    grids[index] << o
+private boolean validSolution(String s) {
+	def strings = s.split("(?!^)")
+	def ints = strings.collect {it as int}
+	
+	rowsValid(ints) && columnsValid(ints) && gridsValid(ints) 
 }
 
-println grids
+private boolean rowsValid(List<Integer> ints) {
+	def rows = ints.collate(9)
+	
+	rows.find {it.sort() != model} == null
+}
 
-grids.each { grid ->
-    valid = valid && grid.sort() == model
+private boolean columnsValid(List<Integer> ints) {
+	def columns = [].withEagerDefault{[]}
+	ints.eachWithIndex {o, i ->
+		int index = i % 9
+		columns[index] << o
+	}
+	columns.find { it.sort() != model} == null
+}
+
+private boolean gridsValid(List<Integer> ints) {
+	def grids = [].withEagerDefault{[]}
+	
+	ints.eachWithIndex {o, i ->
+		int index = i.intdiv(27) * 3  + (i % 9).intdiv(3)
+		grids[index] << o
+	}
+	
+	println grids
+	
+	grids.find { it.sort() != model} == null
 }
 
 println valid
