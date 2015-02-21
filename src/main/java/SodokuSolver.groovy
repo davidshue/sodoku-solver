795ca580c9d5c9caff24d63d1197cbf4849f594f
David Shue <dshue1@babycenter.com>
2015-02-20 17:59:15 -0800

    sodoku-solver

---
+++ src/main/java/SodokuSolver.groovy
@@ -0,0 +1,42 @@
+def model = (1..9)
+
+boolean valid = true
+
+
+String s= '''123456789123456789123456789123456789123456789123456789123456789123456789123456789'''
+
+valid = valid && s ==~ /\d+/ &&  s.length() == 81
+
+def strings = s.split("(?!^)")
+def ints = strings.collect {it as int}
+
+def rows = ints.collate(9)
+
+rows.each { row ->
+    valid = valid && row.sort() == model
+}
+
+def columns = [[],[],[],[],[],[],[],[],[]]
+ints.eachWithIndex {o, i ->
+    int index = i % 9
+    columns[index] << o
+}
+columns.each { column ->
+    valid = valid && column.sort() == model
+}
+
+
+def grids = [[],[],[],[],[],[],[],[],[]]
+
+ints.eachWithIndex {o, i ->
+    int index = i.intdiv(27) * 3  + (i % 9).intdiv(3)
+    grids[index] << o
+}
+
+println grids
+
+grids.each { grid ->
+    valid = valid && grid.sort() == model
+}
+
+println valid
