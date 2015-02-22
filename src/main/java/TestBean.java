94b3d93594812e7a38b060b6ca5ca19562c2a7db
David Shue <dshue1@babycenter.com>
2015-02-21 16:51:04 -0800

    added gradle attributes

---
+++ src/main/java/TestBean.java
@@ -0,0 +1,17 @@
+
+public class TestBean {
+	private String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescripton() {
+		return descripton;
+	}
+	public void setDescripton(String descripton) {
+		this.descripton = descripton;
+	}
+	private String descripton;
+}
